10605ee9f9482cfd1be1219e967eef70f6195624
NRdrgz <137079897+NRdrgz@users.noreply.github.com>
2023-12-11 15:33:22 +0000

    First working version of a simple echo bot

--- Hello.py
+++ Hello.py
@@ -13,12 +13,25 @@ def run():
     st.write("# Welcome to Nicolas Rodriguez's Resume Bot! 👋")
 
 
-    st.markdown(
-        """
-        Hello World
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "Welcome! I'm Nico's Resume Bot, specialized in providing information about Nicolas Rodriguez's professional background and qualifications. Feel free to ask me questions such as:"}]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input():
         
-    """
-    )
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+
+        ##TODO Send message to a LLM and get response
+        #client = OpenAI(api_key=openai_api_key)
+        #response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+        #msg = response.choices[0].message.content
+
+        msg = "ok"
+        st.session_state.messages.append({"role": "assistant", "content": msg})
+        st.chat_message("assistant").write(msg)
 
 
 if __name__ == "__main__":
